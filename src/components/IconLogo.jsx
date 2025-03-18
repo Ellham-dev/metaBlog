27ccc8322ecf317976fbe988e0ff421f3634f749
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-19 00:11:30 +0600

    new componenets complete

---
+++ src/components/IconLogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IconLogo = () => {
+  return (
+    <>
+    <h5 className='bg-[#4B6BFB] px-2.5 py-1 text-white text-sm leading-5 font-medium w-[97px] rounded-lg mb-4'>Technology</h5>
+    </>
+  )
+}
+
+export default IconLogo
