d2be9995ae0f39f96b156cf654844905482533a8
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-18 19:15:06 +0600

    footer about part end

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <section className='bg-[#F6F6F7]'>
+        <div className="max-w-[1216px] mx-auto font-plusjakartasans">
+          <div className="">
+            <div className="w-[280px]">
+              <h6 className='font-semibold text-lg leading-7 mb-3'>About</h6>
+              <p className='text-base leading-6 mb-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+              <h5 className='text-base leading-6 mb-1'><span className='font-semibold'>Email :</span> info@jstemplate.net</h5>
+              <h5 className='text-base leading-6'><span className='font-semibold'>Phone :</span>880 123 456 789</h5>
+            </div>
+            <div className="">
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
