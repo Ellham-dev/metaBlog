8733dc19901fe381cb934f4d798c3e65de027199
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-19 16:51:30 +0600

    singlePost part complete

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import User1 from "/User1.png"
 import IconLogo from '../components/IconLogo'
+import BlogA1 from '/image1.png'
+import BlogA2 from '/BlogA2.png'
+import Ads from '../components/Ads'
 
 
 const SinglePost = () => {
@@ -8,19 +11,59 @@ const SinglePost = () => {
     <>
       <section>
         <div className="max-w-[800px] mx-auto">
-          <div className="">
-            <div className="mt-8">
-              <IconLogo />
-              <h1 className='text-4xl leading-10 font-semibold mb-6'>The Impact of Technology on the Workplace: How Technology is Changing</h1>
-              <div className="gap-x-5 flex items-center">
-                <div className="gap-x-3 flex items-center">
-                  <img src={User1} alt="" />
-                  <p className='text-base leading-6 font-medium text-[#97989F]'>Jason Francisco</p>
-                </div>
-                <span className='text-base leading-6 text-[#97989F]'>August 20, 2024</span>
+          <div className="py-8">
+            <IconLogo />
+            <h1 className='text-4xl leading-10 font-semibold mb-6'>The Impact of Technology on the Workplace: How Technology is Changing</h1>
+            <div className="gap-x-5 flex items-center">
+              <div className="gap-x-3 flex items-center">
+                <img src={User1} alt="" />
+                <p className='text-base leading-6 font-medium text-[#97989F]'>Jason Francisco</p>
               </div>
+              <span className='text-base leading-6 text-[#97989F]'>August 20, 2024</span>
             </div>
-            
+          </div>
+          <div className="">
+            <img src={BlogA1} alt="" className='w-full mb-8' />
+            <p className='text-xl leading-8 mb-8'>Traveling is an enriching experience that opens up new horizons, exposes us to different cultures, and creates memories that last a lifetime. However, traveling can also be stressful and overwhelming, especially if you don't plan and prepare adequately. In this blog article, we'll explore tips and tricks for a memorable journey and how to make the most of your travels.
+
+              One of the most rewarding aspects of traveling is immersing yourself in the local culture and customs. This includes trying local cuisine, attending cultural events and festivals, and interacting with locals. Learning a few phrases in the local language can also go a long way in making connections and showing respect.</p>
+            <div className="">
+              <h4 className='text-2xl leading-7 font-semibold mb-4' >Research Your Destination</h4>
+              <p className='text-xl leading-8 mb-8'>Before embarking on your journey, take the time to research your destination. This includes understanding the local culture, customs, and laws, as well as identifying top attractions, restaurants, and accommodations. Doing so will help you navigate your destination with confidence and avoid any cultural faux pas.
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.</p>
+            </div>
+            <div className="">
+              <h4 className='text-2xl leading-7 font-semibold mb-4' >Plan Your Itinerary</h4>
+              <p className='text-xl leading-8 mb-8'>While it's essential to leave room for spontaneity and unexpected adventures, having a rough itinerary can help you make the most of your time and budget. Identify the must-see sights and experiences and prioritize them according to your interests and preferences. This will help you avoid overscheduling and ensure that you have time to relax and enjoy your journey.
+
+                Vitae sapien pellentesque habitant morbi tristique. Luctus venenatis lectus magna fringilla. Nec ullamcorper sit amet risus nullam eget felis. Tincidunt arcu non sodales neque sodales ut etiam sit amet.</p>
+            </div>
+            <div className="bg-[#F6F6F7] text-2xl leading-8 p-8 text-[#181A2A] rounded-lg mb-8">“ Traveling can expose you to new environments and potential health risks, so it's crucial to take precautions to stay safe and healthy. ”</div>
+            <img src={BlogA2} alt="" className='mb-8 w-full' />
+            <div className="grid place-content-center mb-8">
+              <Ads />
+            </div>
+            <div className="">
+              <h4 className='text-2xl leading-7 font-semibold mb-4' >Pack Lightly and Smartly</h4>
+              <p className='text-xl leading-8 mb-8'>Packing can be a daunting task, but with some careful planning and smart choices, you can pack light and efficiently. Start by making a packing list and sticking to it, focusing on versatile and comfortable clothing that can be mixed and matched. Invest in quality luggage and packing organizers to maximize space and minimize wrinkles.</p>
+            </div>
+            <div className="">
+              <h4 className='text-2xl leading-7 font-semibold mb-4' >Stay Safe and Healthy</h4>
+              <p className='text-xl leading-8 mb-8'>Traveling can expose you to new environments and potential health risks, so it's crucial to take precautions to stay safe and healthy. This includes researching any required vaccinations or medications, staying hydrated, washing your hands frequently, and using sunscreen and insect repellent. It's also essential to keep your valuables safe and secure and to be aware of your surroundings at all times.</p>
+            </div>
+            <div className="">
+              <h4 className='text-2xl leading-7 font-semibold mb-4' >Immerse Yourself in the Local Culture</h4>
+              <p className='text-xl leading-8 mb-8'>One of the most rewarding aspects of traveling is immersing yourself in the local culture and customs. This includes trying local cuisine, attending cultural events and festivals, and interacting with locals. Learning a few phrases in the local language can also go a long way in making connections and showing respect.</p>
+            </div>
+            <div className="">
+              <h4 className='text-2xl leading-7 font-semibold mb-4' >Capture Memories</h4>
+              <p className='text-xl leading-8 mb-8'>Finally, don't forget to capture memories of your journey. Whether it's through photographs, journaling, or souvenirs, preserving the moments and experiences of your travels can bring joy and nostalgia for years to come. However, it's also essential to be present in the moment and not let technology distract you from the beauty of your surroundings.</p>
+            </div>
+            <div className="">
+              <h4 className='text-2xl leading-7 font-semibold mb-4' >Conclusion:</h4>
+              <p className='text-xl leading-8 mb-8'>Traveling is an art form that requires a blend of planning, preparation, and spontaneity. By following these tips and tricks, you can make the most of your journey and create memories that last a lifetime. So pack your bags, embrace the adventure, and enjoy the ride.</p>
+            </div>
           </div>
         </div>
       </section>
